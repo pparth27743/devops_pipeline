67228376d1727f9f650450b090ef3d41679a1521
pparth27743 <pparth27743@gmail.com>
2021-02-20 09:49:10 +0530

    20th Feb 9:49 AM commit

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World !!! ");
+
+        System.out.println("Hello World !!!");
+        Scanner sc= new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a= sc.nextInt();
+        System.out.print("Enter second number: ");
+        int b= sc.nextInt();
+        int d=a+b;
+        System.out.println("Addition of 2 numbers : " + d);
+
     }
 
 }
