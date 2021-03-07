07b007401400f7017fef3f91c38cf9fee1d0d941
Parth Patel <62556601+pparth27743@users.noreply.github.com>
2021-03-07 16:22:43 +0530

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,18 +2,89 @@ import java.util.Scanner;
 
 public class HelloWorld {
 
-    public static void main(String[] args) {
-
-        System.out.println("Hello World !!!");
-        Scanner sc= new Scanner(System.in);
-
-        System.out.print("Enter first number: ");
-        int a= sc.nextInt();
-        System.out.print("Enter second number: ");
-        int b= sc.nextInt();
-        int d=a+b;
-        System.out.println("Addition of 2 numbers : " + d);
-
+    public static void  printAns(double ans){
+        System.out.println("************************************************");
+        System.out.println("Your answer is " + ans);
+        System.out.println("************************************************");
     }
 
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Welcome to Mini Calculator... \nChoose one of the Following operation.");
+        System.out.println("Press 1 for Square Root");
+        System.out.println("Press 2 for Factorial");
+        System.out.println("Press 3 for Natural logarithm");
+        System.out.println("Press 4 for Power function");
+        System.out.println("Press 0 to Exit");
+
+        int choice, num;
+        double ans;
+
+        do {
+            choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter the Number");
+                    num = sc.nextInt();
+
+                    try{
+                        ans = Math.sqrt(num);
+                        printAns(ans);
+                    }catch (Exception e){
+                        System.out.println(e);
+                    }
+
+                    break;
+
+                case 2:
+                    System.out.println("Enter the Number");
+                    num = sc.nextInt();
+                    long sum = 1;
+                    for(int i=2; i<=num; i++) {
+                        sum = sum * i;
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("Enter the Number");
+                    num = sc.nextInt();
+
+                    try{
+                        ans = Math.log(num);
+                        printAns(ans);
+                    }catch (Exception e) {
+                        System.out.println(e);
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("Enter the base number");
+                    num = sc.nextInt();
+                    System.out.println("Enter the power number");
+                    int power = sc.nextInt();
+
+                    try{
+                        ans = Math.pow(num, power);
+                        printAns(ans);
+                    }catch (Exception e) {
+                        System.out.println(e);
+                    }
+                    break;
+
+                case 0:
+                    break;
+
+                default:
+                    System.out.println("Please choose correct option");
+                    break;
+            }
+
+        }while(choice != 0);
+
+    }
 }
+
